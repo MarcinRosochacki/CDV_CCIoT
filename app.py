652f27748f0f2327a55a20f53177754737487013
Marcin Rosochacki <72520400+MarcinRosochacki@users.noreply.github.com>
2024-06-09 23:14:26 +0200

    Update app configuration and requirements

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import pyodbc
 
 app = Flask(__name__)
-
-# Konfiguracja łańcucha połączenia z użyciem Microsoft Entra ID
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'mssql+pyodbc://{user_name}@{server}/{database}?'
-    'driver=ODBC+Driver+18+for+SQL+Server&'
-    'Encrypt=yes&TrustServerCertificate=no&'
-    'Connection Timeout=30&'
-    'Authentication=ActiveDirectoryIntegrated'
-).format(
-    user_name='mrosochacki@edu.cdv.pl',
-    server='cdvsql.database.windows.net',
-    database='playersdb'
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:cdvsql.database.windows.net,1433;Database=playersdb;Uid=mrosochacki@edu.cdv.pl;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryPassword'
 
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 @app.route('/')
 def index():
@@ -28,4 +21,4 @@ def index():
     return render_template('index.html', users=users)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Basiendki\Documents\CDV\AzureLabs\my-flask-app\CDV_CCIoT\my-flask-app\venv
+set VIRTUAL_ENV=C:\Users\Basiendki\Documents\CDV\AzureLabs\CDV_CCIoT\my-flask-app\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
